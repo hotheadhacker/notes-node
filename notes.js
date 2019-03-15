fe1a91b8b0d880df72dcfa748d8f150f5f8b0dca
Salman Qureshi <isalmanqureshi@gmail.com>
2019-03-15 22:03:09 +0530

    Added note insert and saving it into json file

--- notes.js
+++ notes.js
@@ -1,21 +1,38 @@
 console.log('Starting notes.js');
-
+const fs = require('fs');
 
 var addNote = (title,body) => {
-  console.log('Adding Note...',title,body);
-}
+
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+ try{
+   var notesString = fs.readFileSync('notes-data.json');
+   notes = JSON.parse(notesString);
+ } catch(e){
+
+ }
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if(duplicateNotes.length === 0){
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+};
 
 var getAll = () => {
   console.log('Grabbing Notes...');
-}
+};
 
 var read = (title) => {
   console.log('Reading a Note...', title);
-}
+};
 
 var remove = (title) => {
   console.log('Note Removed Successfully...', title);
-}
+};
 module.exports =  {
 addNote,
 getAll,
--- playground/json.js
+++ playground/json.js
@@ -8,7 +8,7 @@
 //
 // console.log(typeof stringObj);
 // console.log(stringObj);
-
+ 
 
 //CONVERTING STRING TO OBJECT
 
